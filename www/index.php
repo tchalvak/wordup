616ce31c37ed3b80b8da8d67b5804de527650f9b
Roy Ronalds <tchalvakspam@gmail.com>
2014-07-22 15:32:27 -0400

    Templates: Added a templates directory, and moved the main bit of html to it.  Separate file for html, the main php, the php functions, and the css.

--- www/index.php
+++ www/index.php
@@ -1,5 +1,6 @@
 <?php
-require_once(__DIR__.'/../core/lib_wordup.php');
+DEFINE('ROOT', __DIR__.'/../'); // Defined the path to the ROOT directory for reuse, usually you'll get this defined elsewhere from the start.
+require_once(ROOT.'core/lib_wordup.php');
 // All the php functions are now in the separate core/lib_wordup.php
 
 
@@ -25,57 +26,6 @@ if($theQuery){ // Just for the purposes of this example, let's move the if here 
   $query_present = 'query-present';
 }
 
-?>
 
-<html>
- <head>
- <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
- <title>WordUp</title>
- <link rel="stylesheet" type="text/css" href="/css/styles.css">
- <!-- CSS moved to external stylesheet -->
-</head>
-
-<body>
-
-<table>
- <tr>
-  <td></td>
-  <td width=800px>
-
-    <table class=topTable height=80px>
-     <tr height=30px><td></td></tr>
-     <tr height=32px valign=top>
-      <td width=525px>
-        <?php echo $theWord; ?>
-      </td>
-      <td align=right width=225px>
-        <form method="get"><input type="text" name="q" style="width:200px; height:32px;" />
-      </td>
-      <td>
-        <img src='img/icon_info_1.svg' style='height: 32px; width: 32px;' />
-      </td>
-     </tr>
-     <tr height=18px><td></td></tr>
-    </table>
-
-    <table height=400px class='<?php echo $query_present;?>'>
-     <tr align=left valign=top>
-      <td width=260px class='definitionBox'>
-        <?php echo $dictionary_section; // Notably unescaped, which has it's dangers ?>
-      </td>
-      <td width=10px></td>
-      <td width=260px class='definitionBox'>
-       <?php echo $slang_section; // Again, naked dynamic html here ?>
-      </td>
-      <td width=10px></td>
-      <td width=260px class='definitionBox'>
-       <?php echo $thesaurus_section; // Naked dynamic html again ?>
-      </td>
-    </tr>
-   </table>
-
-  </td>
-  <td></td>
- </tr>
-</table>
-</body>
+// Here we use a simple, simple approach to separate php files as a template!
+include(ROOT.'templates/main.php'); 
